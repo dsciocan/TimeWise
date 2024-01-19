8f90a2f07677454313a5ab87cfbe671ab1ef07dd
emeritolopes <emerito.lopes@gmail.com>
2024-01-19 20:30:14 +0000

    added views on calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'
 import daygridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
 
-
+//adding views
 function Calendars() {
     return (
         <div>
-            <FullCalendar plugins={[daygridPlugin]} />
+            <FullCalendar 
+            plugins={[daygridPlugin, timeGridPlugin,interactionPlugin]}
+             initialView={'dayGridMonth'}
+             headerToolbar={{
+                start:"today prev next",
+                center:"title",
+                end:"dayGridMonth, timeGridWeek, timeGridDay",
+                
+             }}
+                />
         </div>
-    )
+    );
 }
 
-export default Calendars;
 
+
+export default Calendars;
 
 
 
