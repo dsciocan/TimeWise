947c61427581b34d17b952cfe97bb1495c4c9fb9
borsa <129904894+Borsa8@users.noreply.github.com>
2024-01-23 01:53:50 +0000

    addnote handleChange added

--- src/components/Notes/AddNote.jsx
+++ src/components/Notes/AddNote.jsx
@@ -4,11 +4,17 @@ const AddNote = ({ handleAddNote }) => {
   // Initial Id
   const id = useId();
 
-  // Create a unique ID from the initial Id
-  const [index, setIndex] = useState(0);
+// Create a unique ID from the initial Id
+const [index, setIndex] = useState(0);
 
-  const [noteText, setNoteText] = useState("");
-  const characterLimit = 200;
+const [noteText, setNoteText] = useState("");
+const characterLimit = 200;
+
+const handleChange = (event) => {
+  if (characterLimit - event.target.value.length >= 0) {
+    setNoteText(event.target.value);
+  }
+};
 
 
 
