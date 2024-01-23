533e988ab760f769001c3bbe5b09eb720c73de97
borsa <129904894+Borsa8@users.noreply.github.com>
2024-01-23 01:58:22 +0000

    addnote handleSave added

--- src/components/Notes/AddNote.jsx
+++ src/components/Notes/AddNote.jsx
@@ -4,17 +4,26 @@ const AddNote = ({ handleAddNote }) => {
   // Initial Id
   const id = useId();
 
-// Create a unique ID from the initial Id
-const [index, setIndex] = useState(0);
+  // Create a unique Id from the initial Id
+  const [index, setIndex] = useState(0);
 
-const [noteText, setNoteText] = useState("");
-const characterLimit = 200;
+  const [noteText, setNoteText] = useState("");
+  const characterLimit = 200;
 
-const handleChange = (event) => {
-  if (characterLimit - event.target.value.length >= 0) {
-    setNoteText(event.target.value);
-  }
-};
+  const handleChange = (event) => {
+    if (characterLimit - event.target.value.length >= 0) {
+      setNoteText(event.target.value);
+    }
+  };
+
+  const handleSaveClick = () => {
+    if (noteText.trim().length > 0) {
+      handleAddNote(noteText, id + index);
+      setNoteText("");
+      // A unique Id in each save event
+      setIndex(index + 1);
+    }
+  };
 
 
 
